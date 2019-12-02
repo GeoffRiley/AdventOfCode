ed89f3ac6c7b6f157ff78ce68a98e1b2cda51f2b
Geoff Riley <geoffr@adaso.com>
2019-12-02 06:31:58 +0000

    Day 2 solution, with explanation of 'code' function

--- 02/test_computer.py
+++ 02/test_computer.py
@@ -2,7 +2,8 @@ import pytest
 
 from computer import computer
 
-@pytest.mark.parametrize('cells, result',[
+
+@pytest.mark.parametrize('cells, result', [
     ('1,0,0,0,99', '2,0,0,0,99'),
     ('2,3,0,3,99', '2,3,0,6,99'),
     ('2,4,4,5,99,0', '2,4,4,5,99,9801'),
@@ -10,4 +11,3 @@ from computer import computer
 ])
 def test_computer(cells, result):
     assert computer(cells) == result
-
