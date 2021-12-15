d3b89d3ad6730eaeca697e9e3b4f538b26a006e8
Geoff Riley <gir6@ou.ac.uk>
2021-12-15 17:36:30 +0000

    Added Grid class to library

---
+++ aoc/grid.py
@@ -0,0 +1,93 @@
+class Grid:
+    def __init__(self, default=0):
+        self.grid = {}
+        self.default = default
+        self._ranges = {}
+
+    def copy(self):
+        ret = Grid()
+        ret.grid = self.grid.copy()
+        ret.default = self.default
+        return ret
+
+    @staticmethod
+    def from_text(values):
+        grid = Grid()
+        if isinstance(values, str):
+            if "\n" in values:
+                values = values.split("\n")
+            else:
+                values = [values]
+        for y, row in enumerate(values):
+            for x, cur in enumerate(row):
+                grid.set(cur, x, y)
+        return grid
+
+    def __getitem__(self, key):
+        if isinstance(key, tuple):
+            return self.grid.get(key, self.default)
+        else:
+            return self.grid.get((key,), self.default)
+
+    def __setitem__(self, key, value):
+        self._ranges = {}
+        if isinstance(key, tuple):
+            self.grid[key] = value
+        else:
+            self.grid[(key,)] = value
+
+    def __delitem__(self, key):
+        if isinstance(key, tuple):
+            del self.grid[key]
+        else:
+            del self.grid[(key,)]
+
+    def __iter__(self):
+        return self.grid.values().__iter__()
+
+    def __contains__(self, key):
+        if isinstance(key, tuple):
+            return key in self.grid
+        else:
+            return (key,) in self.grid
+
+    def get(self, *coords):
+        return self.grid.get(coords, self.default)
+
+    def set(self, value, *coords):
+        self._ranges = {}
+        self.grid[coords] = value
+
+    def axis_min(self, axis):
+        if self._ranges.get(axis, None) is None:
+            self._ranges[axis] = (min([x[axis] for x in self.grid]), max([x[axis] for x in self.grid]))
+        return self._ranges[axis][0]
+
+    def axis_max(self, axis):
+        if self._ranges.get(axis, None) is None:
+            self._ranges[axis] = (min([x[axis] for x in self.grid]), max([x[axis] for x in self.grid]))
+        return self._ranges[axis][1]
+
+    def axis_size(self, axis):
+        return self.axis_max(axis) - self.axis_min(axis) + 1
+
+    def width(self):
+        return self.axis_size(0)
+
+    def height(self):
+        return self.axis_size(1)
+
+    def neighbors(self, *args, diagonals=False, valid_only=False):
+        if isinstance(args[0], tuple):
+            x, y = args[0]
+        else:
+            x, y = args
+        if diagonals:
+            offsets = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+        else:
+            offsets = ((0, -1), (1, 0), (0, 1), (-1, 0))
+        for ox, oy in offsets:
+            ox = ox + x
+            oy = oy + y
+            if not valid_only or (ox, oy) in self.grid:
+                yield ox, oy
