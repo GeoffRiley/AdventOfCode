fbf3fdc9730a0fc23ebffa134c1445cd48cebc5b
Geoff Riley <geoffr@adaso.com>
2023-12-04 04:57:47 +0000

    Library fix in grid.py
    Added a new function to the grid.py library to automate the scaning
    through the grid. The function is called `next_location`. It takes
    the current location and the grid as arguments and returns the next
    location in the grid scaning from left to right and top to bottom.
    If the end of the grid is reached, the function returns None, unless
    the `wrap` argument is set to True, in which case the function returns
    the first location in the grid.
    
    Additionally, the `get` and `set` functions have been modified to
    accept a tuple as the location argument. This allows the efficient
    use of the `next_location` function.

--- aoc/grid.py
+++ aoc/grid.py
@@ -52,11 +52,11 @@ class Grid:
             return (key,) in self.grid
 
     def get(self, *coords):
-        return self.grid.get(coords, self.default)
+        return self.grid.get(*coords, self.default)
 
     def set(self, value, *coords):
         self._ranges = {}
-        self.grid[coords] = value
+        self.grid[*coords] = value
 
     def axis_min(self, axis):
         if self._ranges.get(axis, None) is None:
@@ -78,6 +78,11 @@ class Grid:
         return self.axis_size(1)
 
     def neighbors(self, *args, diagonals=False, valid_only=False):
+        """
+        Returns the neighbors of the given location.
+        If diagonals is True, returns the 8 neighbors of the given location.
+        If valid_only is True, only returns neighbors that are in the grid.
+        """
         if isinstance(args[0], tuple):
             x, y = args[0]
         else:
@@ -91,3 +96,24 @@ class Grid:
             oy = oy + y
             if not valid_only or (ox, oy) in self.grid:
                 yield ox, oy
+
+    def next_location(self, location, wrap=False):
+        """
+        Returns the next location in the grid, or None if there is no next location.
+        If wrap is True, the grid wraps around.
+        """
+        if isinstance(location, tuple):
+            x, y = location
+        else:
+            x, y = location.x, location.y
+        if x < self.axis_max(0):
+            x += 1
+        elif y < self.axis_max(1):
+            x = self.axis_min(0)
+            y += 1
+        elif wrap:
+            x = self.axis_min(0)
+            y = self.axis_min(1)
+        else:
+            return None
+        return x, y
