420ad0523efb254003c44d4f8cbcdd10b6ca0deb
Geoff Riley <geoffr@adaso.com>
2023-12-16 20:27:03 +0000

    Day 14, Part 2: Parabolic Reflector Dish
    Part 1:
    Time    Rank  Score
    >24h   29661      0
    
    Part 2:
    Time    Rank  Score
    >24h  28062      0

--- 2023/14/day14.py
+++ 2023/14/day14.py
@@ -2,33 +2,35 @@
 Advent of code 2023
 Day 14: Parabolic Reflector Dish
 """
+from copy import deepcopy
+from functools import cache
 from pprint import pprint
 from textwrap import dedent
 
 from aoc.loader import LoaderLib
 from aoc.utility import lines_to_list
-from grid import Grid
 
 
-def print_grid(grid):
+def print_grid(grid: tuple[str]) -> None:
     # print the grid
     xlate = {
         "O": "O",
         "#": ".",
         ".": " ",
     }
-    print("-" * (grid.width()))
-    for row in range(grid.height()):
-        for col in range(grid.width()):
-            if grid[col, row] in xlate:
-                print(xlate[grid[col, row]], end="")
+    print("-" * (len(grid[0])))
+    for row in grid:
+        for char in row:
+            if char in xlate:
+                print(xlate[char], end="")
             else:
-                print(grid[col, row], end="")
+                print(char, end="")
         print()
-    print("-" * (grid.width()))
+    print("-" * (len(grid[0])))
 
 
-def tilt_north(grid: Grid) -> None:
+@cache
+def tilt_west(grid: tuple[str]) -> tuple[str]:
     """
     Move all boulders northward as far as possible.
 
@@ -39,34 +41,82 @@ def tilt_north(grid: Grid) -> None:
     """
     # print("before")
     # print_grid(grid)
-    for col in range(grid.width()):
-        available_cell = False
-        for row in range(grid.height()):
-            character = grid[col, row]
-            if character == ".":
-                if not available_cell:
-                    available_cell = (col, row)
-                    grid[available_cell] = "×"
-                    # print("available cell found")
-                    # print_grid(grid)
-            elif character == "#":
-                if available_cell:
-                    grid[available_cell] = "."
-                available_cell = False
-            elif character == "O":
-                if available_cell:
-                    grid[col, row] = "."
-                    grid[available_cell] = character
-                    available_cell = (available_cell[0], available_cell[1]+1)
-                    grid[available_cell] = "×"
-                    # print("move boulder")
-                    # print_grid(grid)
-    # print("after")
+    new_grid = list(grid)
+    for grid_idx, line in enumerate(grid):
+        # Form the 'O' and '.' into groups separated by '#'
+        line = "".join(line).split("#")
+        line_copy = deepcopy(line)
+        for idx, section in enumerate(line_copy):
+            if section != "":
+                line_copy[idx] = "".join(
+                    # Sorting each section will move the 'O' to the end,
+                    # reversing that will move them to the front.
+                    sorted(section, reverse=True)
+                )
+        # Rejoin the line parts with #s in between
+        new_grid[grid_idx] = "#".join(line_copy)
+    return tuple(new_grid)
+
+
+@cache
+def tilt_north(grid: tuple[str]) -> tuple[str]:
+    """
+    Move all boulders northward as far as possible.
+
+    Scan across the grid column by column from north to south. For each
+    boulder found, move it northward as far as possible. If it hits an
+    immovable object, it stops moving. If it hits another boulder, it stops
+    moving. If it hits the edge of the grid, it stops moving.
+    """
+    # print("*** before ***")
     # print_grid(grid)
-    # print()
+    new_grid = rotate(grid)
+    # print("first rotate")
+    # print_grid(new_grid)
+    new_grid = tilt_west(new_grid)
+    # print("after tilt west")
+    # print_grid(new_grid)
+    new_grid = rotate(new_grid, clockwise=True)
+    # print("after second rotate")
+    # print_grid(new_grid)
+    return new_grid
 
 
-def part1(lines: list[list[str]]) -> int:
+@cache
+def rotate(curr_map: tuple[str], clockwise: bool = False) -> tuple[str]:
+    # Reverse all lines and return the columns to simulate a single rotation
+    if clockwise:
+        return tuple(["".join(reversed(line)) for line in zip(*curr_map)])
+    return tuple(["".join(line) for line in zip(*map(reversed, curr_map))])
+
+
+@cache
+def do_field_spin(grid: tuple[str]) -> tuple[str]:
+    # tilt the grid north: move all boulders northward as far as possible.
+    for _ in range(4):
+        grid = rotate(tilt_north(grid), clockwise=True)
+        # print(f"set {_} = {calc_mass_effect(grid)}")
+        # print_grid(grid)
+    return grid
+
+
+@cache
+def do_1000_spin(grid: tuple[str]) -> tuple[str]:
+    # tilt the grid north: move all boulders northward as far as possible.
+    for _ in range(1_000):
+        grid = do_field_spin(grid)
+        # print(f"set {_} = {calc_mass_effect(grid)}")
+        # print_grid(grid)
+    return grid
+
+
+def calc_mass_effect(grid: tuple[str]) -> int:
+    # calculate the total mass effect of all boulders in the grid
+    height = len(grid)
+    return sum(row.count('O') * (height - y) for y, row in enumerate(grid))
+
+
+def part1(lines: list[str]) -> int:
     """
     Boulders, "O", move around the grid in a stright line until they hit an
     immovable object, "#", am already stationary boulder or the edge of the
@@ -78,22 +128,28 @@ def part1(lines: list[list[str]]) -> int:
     grid after the grid has been tilted north an all boulders have come to
     rest.
     """
-    grid = Grid.from_text(lines)
+    grid = tuple(lines)
     # tilt the grid north: move all boulders northward as far as possible.
-    tilt_north(grid)
+    grid = tilt_north(grid)
     # calculate the total mass effect of all boulders in the grid
-    total_mass_effect = 0
-    for row in range(grid.height()):
-        for col in range(grid.width()):
-            if grid[col, row] == "O":
-                total_mass_effect += grid.height() - row
-    return total_mass_effect
+    return calc_mass_effect(grid)
 
 
-def part2(lines) -> int:
+def part2(lines: list[str]) -> int:
     """
+    We are required to test the weight of the boulders in the grid after
+    1_000_000_000 iterations of the cycle funtion. The cycle function tilts
+    the grid north and then west and then south and then east. After each
+    tilt, the boulders are allowed to come to rest before the next tilt.
     """
-    ...
+    grid = tuple(lines)
+
+    for _ in range(1_000_000):
+        grid = do_1000_spin(grid)
+        # print(f"set {_} = {calc_mass_effect(grid)}")
+        # print_grid(grid)
+
+    return calc_mass_effect(grid)
 
 
 def main():
