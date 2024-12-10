c3cd4379e00609b6d70468b78ebc97dc925aad02
Geoff Riley <geoffr@adaso.com>
2024-12-10 10:34:55 +0000

    2024 Day 09: Disk Fragmenter Done!
    At last.

--- 2024/09/day09.py
+++ 2024/09/day09.py
@@ -15,14 +15,14 @@ def part1(input_text: str) -> int:
     file_id = 0
     current_pos = 0
     for ch in input_text:
-        l = int(ch)
+        block_len = int(ch)
         if is_file:
-            disc_layout.update(
-                {j: file_id for j in range(current_pos, current_pos + l)}
-            )
+            disc_layout |= {
+                j: file_id for j in range(current_pos, current_pos + block_len)
+            }
             file_id += 1
         is_file = not is_file
-        current_pos += l
+        current_pos += block_len
 
     keyset = set(disc_layout.keys())
     free_empty = set(range(max(keyset))) - keyset
@@ -34,12 +34,7 @@ def part1(input_text: str) -> int:
         if fs < src:
             disc_layout[fs] = disc_layout.pop(src)
 
-    total = 0
-    # for i,x in enumerate(sorted(disc_layout.items(), key=lambda x:x[0])):
-    for i, x in disc_layout.items():
-        total += i * x
-
-    return total
+    return sum(i * x for i, x in disc_layout.items())
 
 
 def part2(input_text: list[str]) -> int:
@@ -51,18 +46,18 @@ def part2(input_text: list[str]) -> int:
     file_id = 0
     current_pos = 0
     for ch in input_text:
-        l = int(ch)
+        block_len = int(ch)
         if is_file:
-            if l <= 0:
+            if block_len <= 0:
                 raise ValueError("File size must be positive")
-            file_space[current_pos] = (file_id, l)
-            file_dir[file_id] = (current_pos, l)
+            file_space[current_pos] = (file_id, block_len)
+            file_dir[file_id] = (current_pos, block_len)
             file_id += 1
         else:
-            if l > 0:
-                free_space[current_pos] = l
+            if block_len > 0:
+                free_space[current_pos] = block_len
         is_file = not is_file
-        current_pos += l
+        current_pos += block_len
 
     # - Iterate over files in descending order of their ID.
     # - For each file, look for the earliest suitable free space block before
@@ -71,28 +66,40 @@ def part2(input_text: list[str]) -> int:
     # - After processing all files, compute the checksum using the update
     #   positions.
 
-    for fid in sorted(file_space.keys(), reverse=True):
-        fpos, fsize = file_space[fid]
+    for fid in sorted(file_dir.keys(), reverse=True):
+        fpos, fsize = file_dir[fid]
         for fpos_check in sorted(free_space.keys()):
             # Check if the free space is above the file
             if fpos_check > fpos:
                 break
             # Check if the freespace is large enough for the file
-            if free_space[fpos_check] >= fsize:
+            if (freesize := free_space[fpos_check]) >= fsize:
                 # Reduce freespace size, increase pointer position
                 # Move file to where the freespace was
                 # Update freespace pointers
-                free_space[fpos_check] += fsize
-                free_space.pop(fpos)
-                file_dir[fid] = fpos_check
+                if freesize == fsize:
+                    free_space.pop(fpos_check)
+                else:
+                    new_free_space = freesize - fsize
+                    new_free_pos = fpos_check + fsize
+                    free_space.pop(fpos_check)
+                    free_space[new_free_pos] = new_free_space
+                file_dir.pop(fid)
+                file_dir[fid] = (fpos_check, fsize)
+                file_space.pop(fpos)
+                file_space[fpos_check] = (fid, fsize)
                 break
 
-    raise NotImplementedError
+    sub_totals = [
+        sum(i * x for x in range(base, base + sz))
+        for base, (i, sz) in file_space.items()
+    ]
+    return sum(sub_totals)
 
 
 def main():
     loader = LoaderLib(2024)
-    testing = True
+    testing = False
     if not testing:
         input_text = loader.get_aoc_input(9)
     else:
@@ -135,13 +142,19 @@ if __name__ == "__main__":
     main()
     # --
     # --------------------------------------------------------------------------------
-    # LAP -> 0.372609        |        0.372609 <- ELAPSED
+    # LAP -> 0.001307        |        0.001307 <- ELAPSED
     # --------------------------------------------------------------------------------
     # Part setup : len(input_text)=19999 ...
     # --------------------------------------------------------------------------------
 
     # --------------------------------------------------------------------------------
-    # LAP -> 26.816726       |       27.189334 <- ELAPSED
+    # LAP -> 13.832093       |       13.833400 <- ELAPSED
     # --------------------------------------------------------------------------------
     # Part 1   : 6461289671426
+    # --------------------------------------------------------------------------------
+
+    # --------------------------------------------------------------------------------
+    # LAP -> 0.395257        |       14.228657 <- ELAPSED
+    # --------------------------------------------------------------------------------
+    # Part 2   : 6488291456470
     # --------------------------------------------------------------------------------
