05eca0c167dd2a3077955c101f2a2801d803dbc9
Geoff Riley <geoffr@adaso.com>
2024-12-11 01:45:03 +0000

    2024 Day 10  Hoof It, all done

--- 2024/10/day10.py
+++ 2024/10/day10.py
@@ -38,28 +38,43 @@ def bfs_count_trails(map: Grid, start: tuple[int, int]):
         for nx, ny in map.neighbors(x, y, valid_only=True):
             if map[nx, ny] == current_height + 1:
                 queue.append((nx, ny, current_height + 1))
-        # for dx, dy in directions:
-        #     nx, ny = x + dx, y + dy
-        #     if 0 <= nx < rows and 0 <= ny < cols and (nx, ny) not in visited:
-        #         if map[nx][ny] == current_height + 1:
-        #             queue.append((nx, ny, current_height + 1))
 
     return trail_count
 
 
-def part1(lines: list[str]) -> int:
+def bfs_count_distinct_paths(map: Grid, start: tuple[int, int]):
+    # rows, cols = len(map), len(map[0])
+    # directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    paths_count = {}
+    queue = deque(
+        [(start[0], start[1], 0, frozenset())]
+    )  # (row, col, current_height, path)
+
+    while queue:
+        x, y, current_height, path = queue.popleft()
+        path = path | {(x, y)}  # Add the current position to the path
+
+        if map[x, y] == 9:
+            paths_count[path] = 1
+            continue
+
+        for nx, ny in map.neighbors(x, y, valid_only=True):
+            if map[nx, ny] == current_height + 1:
+                queue.append((nx, ny, current_height + 1, path))
+
+    return len(paths_count)
+
+
+def part1(grid: Grid) -> int:
     """ """
-    grid = Grid().from_text(lines)
-    for x in range(grid.width()):
-        for y in range(grid.height()):
-            grid[x, y] = int(grid[x, y])
     trailheads = find_trailheads(grid)
     return sum(bfs_count_trails(grid, trailhead) for trailhead in trailheads)
 
 
-def part2(lines: list[str]) -> int:
+def part2(grid: Grid) -> int:
     """ """
-    ...
+    trailheads = find_trailheads(grid)
+    return sum(bfs_count_distinct_paths(grid, trailhead) for trailhead in trailheads)
 
 
 def main():
@@ -86,11 +101,16 @@ def main():
     # lines = [lines_to_list(line) for line in input_text.split("\n\n")]
     lines = lines_to_list(input_text)
 
+    grid = Grid().from_text(lines)
+    for x in range(grid.width()):
+        for y in range(grid.height()):
+            grid[x, y] = int(grid[x, y])
+
     loader.print_solution("setup", f"{len(lines)=} ...")
     loader.print_solution(
         1,
         part1(
-            lines,
+            grid,
         ),
     )
 
@@ -104,7 +124,7 @@ def main():
     loader.print_solution(
         2,
         part2(
-            lines,
+            grid,
         ),
     )
 
@@ -112,4 +132,20 @@ def main():
 if __name__ == "__main__":
     main()
     # --
-    # -
+    # --------------------------------------------------------------------------------
+    # LAP -> 0.008575        |        0.008575 <- ELAPSED
+    # --------------------------------------------------------------------------------
+    # Part setup : len(lines)=58 ...
+    # --------------------------------------------------------------------------------
+
+    # --------------------------------------------------------------------------------
+    # LAP -> 0.006137        |        0.014712 <- ELAPSED
+    # --------------------------------------------------------------------------------
+    # Part 1   : 760
+    # --------------------------------------------------------------------------------
+
+    # --------------------------------------------------------------------------------
+    # LAP -> 0.010780        |        0.025493 <- ELAPSED
+    # --------------------------------------------------------------------------------
+    # Part 2   : 1764
+    # --------------------------------------------------------------------------------
